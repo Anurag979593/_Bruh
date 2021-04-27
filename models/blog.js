c686bcfe6ae89015508456472cc402933bfdbf30
Anurag <47553679+Anurag979593@users.noreply.github.com>
2021-04-27 18:37:40 +0530

    Updated blogs page

--- models/blog.js
+++ models/blog.js
@@ -19,6 +19,10 @@ const blogSchema = Schema({
     type: String,
     required: true,
   },
+  preview: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
